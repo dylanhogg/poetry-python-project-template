44b0347407793458fd505aa7ca4b6b737eea2228
Dylan Hogg <dylanhogg@gmail.com>
2024-02-05 21:52:14 +1100

    Fix up app exit condition.

--- your_project_name/console.py
+++ your_project_name/console.py
@@ -32,16 +32,16 @@ def run(
     """
     Command entry point
     """
-    log.configure()
-
-    example_usage = f"Example usage: [bold green]{consts.package_name}[/bold green]"
-
-    logger.info(f"Start {consts.package_name}, required_arg = {required_arg}, optional_arg = {optional_arg}")
-    logger.info(f"PYTHONPATH = {env.get('PYTHONPATH', 'Not set')}")
-    logger.info(f"LOG_STDERR_LEVEL = {env.get('LOG_STDERR_LEVEL', 'Not set. Copy `.env_template` to `.env`')}")
-    logger.info(f"LOG_FILE_LEVEL = {env.get('LOG_FILE_LEVEL', 'Not set. Copy `.env_template` to `.env`')}")
 
     try:
+        log.configure()
+        example_usage = f"Example usage: [bold green]{consts.package_name}[/bold green]"
+
+        logger.info(f"Start {consts.package_name}, required_arg = {required_arg}, optional_arg = {optional_arg}")
+        logger.info(f"PYTHONPATH = {env.get('PYTHONPATH', 'Not set')}")
+        logger.info(f"LOG_STDERR_LEVEL = {env.get('LOG_STDERR_LEVEL', 'Not set. Copy `.env_template` to `.env`')}")
+        logger.info(f"LOG_FILE_LEVEL = {env.get('LOG_FILE_LEVEL', 'Not set. Copy `.env_template` to `.env`')}")
+
         start = datetime.now()
 
         print(f"Hello! required_arg = '{required_arg}', optional_arg = '{optional_arg}'")
@@ -53,21 +53,33 @@ def run(
 
         took = datetime.now() - start
         print("")
-        print(f"[bold green]your_project_name finished, took {took.total_seconds()}s.[/bold green]")
-        print("")
-        print(
-            "Thank you for using your_project_name! Please consider starring the project on github: https://github.com/dylanhogg/your_project_name"
-        )
+        print(f"[bold green]{consts.package_name} finished, took {took.total_seconds()}s.[/bold green]")
+
+        raise typer.Exit(0)
 
     except AppUsageException as ex:
         print(example_usage)
         print(f"[bold red]{str(ex)}[/bold red]")
         print("")
         print(f"For more information, try '{consts.package_name} --help'.")
-        raise typer.Exit(code=1) from ex
+        logger.exception(ex)
+
+    except typer.Exit as ex:
+        if ex.exit_code == 0:
+            print()
+            print(
+                f"[bold green]Goodbye and thanks for using {consts.package_name}! Please consider starring the project on github: https://github.com/dylanhogg/{consts.package_name}[/bold green]"
+            )
+            return
+        print(example_usage)
+        print(f"[bold red]Unexpected error code: {str(ex)}[/bold red]")
+        print("")
+        print(f"For more information, try '{consts.package_name} --help'.")
+        logger.exception(ex)
+
     except Exception as ex:
         print(example_usage)
         print(f"[bold red]Unexpected exception: {str(ex)}[/bold red]")
         print("")
         print(f"For more information, try '{consts.package_name} --help'.")
-        raise typer.Exit(code=100) from ex
+        logger.exception(ex)
